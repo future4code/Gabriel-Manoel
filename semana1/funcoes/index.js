e0bb2ba24ebe2ed4fada987efb219cce20173fe4
gabrielmanoel-hub <gabrielmanoel1401@outlook.com>
2021-08-12 16:24:54 -0300

    exercicios-funcoes

---
+++ semana1/funcoes/index.js
@@ -0,0 +1,131 @@
+// ================== Exercícios de interpretação de código ==============
+
+// ============= Exercicio 01 ====================
+// A) Resposta ==> 10, Resposta ==> 50
+// B) Resposta ==> nenhuma resposta no console
+// function minhaFuncao(variavel){
+//     return  variavel * 5
+// }
+
+// console.log(minhaFuncao(2))
+// console.log(minhaFuncao(10))
+// minhaFuncao(2)
+// minhaFuncao(10)
+
+// ============= Exercicio 02 ====================
+/* A) Resposta ==> Verifica se o usuário inseriu o que foi solicitado.
+Uma das utilidades desse comando seria para verificar se um cadastro foi preechido de acordo
+*/
+// B) Resposta ==> i = true, ii = true, iii = true
+
+// let textoDoUsuario = prompt('Insira um texto')
+
+// const outraFuncao = function(texto){
+//     return texto.toLowerCase().includes('cenoura')
+// }
+
+// const resposta = outraFuncao(textoDoUsuario)
+// console.log(resposta)
+
+
+
+
+// ================== Exercícios de escrita de código ==============
+
+
+// ============= Exercicio 01 ====================
+
+// A)
+// const nome = 'gabriel'
+// const idade = 30
+// const estuda = 'estudante'
+
+// const informacao = () =>{
+//     let  mensagem1 = `Eu sou ${nome}, tenho ${idade} anos, moro no Rio de janeiro e sou ${estuda}.`
+//     return resposta = mensagem1
+// }
+
+// console.log(informacao())
+
+
+
+// B)
+// const nome = prompt('Qual é o seu nome?')
+// let idade = Number(prompt('Qual a sua idade?'))
+// const cidade = prompt('Qual é a sua cidade?')
+// const profissao = prompt('Qual é sua profissão?')
+
+
+// const dados = () => {
+//     mensagem = `Eu sou ${nome}, tenho ${idade.toString()} anos, moro em ${cidade} e sou ${profissao}.`
+//     return mensagem
+// }
+
+// console.log(dados())
+
+
+
+// ============= Exercicio 02 ====================
+
+
+//A)
+// let  numero = (numero1, numero2) => {
+//   return numero1 + numero2
+// }
+
+// console.log(numero(2, 2))
+
+
+//B)
+// const valor = (num1, num2) =>{
+//   resposta =  num1 <= num2
+//   return resposta
+// }
+
+// console.log(valor(5, 6))
+
+
+//C)
+// const maisUmValor = (a1) => {
+//    calculo = a1 % 2
+//    resposta = `${calculo === 0}par`
+//    resposta = `${calculo > 0} impar`
+//    return resposta 
+// }
+
+// console.log(maisUmValor(4))
+
+//D)
+// const mensagem = () => {
+//     resposta = 'Ola Mundo!!'
+//     resposta = `Tamanho ${resposta.length}, ${resposta.toUpperCase()}`
+//     return resposta
+// }
+
+// console.log(mensagem())
+
+
+// ============= Exercicio 03 ====================
+let num1 = Number(prompt('Digite um número'))
+let num2 = Number(prompt('Digite outro número'))
+
+const soma = () => {
+    resposta = num1 + num2
+    return resposta
+}
+
+const subtracao = () => {
+    reposta = num1 * num2
+    return resposta
+}
+
+const multiplicacão = () => {
+    
+}
+
+const divisão = () => {
+    
+}
+
+//console.log(`Soma: ${soma()}`)
+console.log(`Diferença: ${subtracao()}`)
