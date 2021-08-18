3a846c9f0cb426fecc928c9b9031644c04ebbf40
gabrielmanoel-hub <gabrielmanoel1401@outlook.com>
2021-08-18 17:03:06 -0300

    exercico-loops

---
+++ semana2/loops/index.js
@@ -0,0 +1,11 @@
+// =================== Exercícios de interpretação de código =====================
+
+// 1) resposta ==> 10
+/* 2) A - resposta ==> 19, 21, 23, 25, 27, 30.
+      B - resposta ==> tiraria o (if) e mudaria para (numero += 0)
+*/
+// 3) ?
+// =================== Exercícios de escrita de código =======================
+
+// ================ Exercicio 01 =================
+// A)
