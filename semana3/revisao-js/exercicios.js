dfdf3c2f0e4043489df65d0f6aa6b15b4a6eee3b
gabrielmanoel-hub <gabrielmanoel1401@outlook.com>
2021-08-23 21:02:16 -0300

    exercicios

--- semana3/revisao-js/exercicios.js
+++ semana3/revisao-js/exercicios.js
@@ -11,37 +11,78 @@ function retornaTamanhoArray(array) {
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-  
+  resposta = array.reverse()
+  return resposta
 }
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  
+    
+     resposta = array.sort(ordena)
+    function ordena (a,b){
+        return a - b
+    }
+    return resposta
+    
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  
+    let resposta = []
+    for(novaLista of array){
+        if(novaLista % 2 === 0){
+            resposta.push(novaLista)
+        }
+    }
+    return resposta
+
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+    let resposta = []
+    
+    for(novaLista of array){
+        if(novaLista % 2 === 0){
+            novaLista **= 2
+            resposta.push(novaLista)
+            
+        }
+    }
+    return resposta 
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+  resposta = Math.max(...array)
+  return resposta
 }
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
+    let maior = Math.max(num1,num2)
+    let menor = Math.min(num1,num2)
+   let  maiorDivisivelPorMenor = maior % menor === 0
 
+const num = {
+    maiorNumero: maior,
+    maiorDivisivelPorMenor: maiorDivisivelPorMenor,
+    diferenca : maior - menor,
+    
+}
+return num
 }
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+    resposta = []
+    for (novaLista of n){
+       if(novaLista.length % 2 === 0){
+        resposta.push(novaLista)
+       }      
+
+   }
+   return resposta
 }
 
 // EXERCÍCIO 09
