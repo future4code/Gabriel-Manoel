3f01c4b48800d87f0dbeb1497384a9df4d00e2d3
gabrielmanoel-hub <gabrielmanoel1401@outlook.com>
2021-09-21 17:22:53 -0300

    auala25

---
+++ semana7/integracao-de-api/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+import Userlist from './components/UserList';
+import Register from './components/Register';
+function App() {
+  return (
+    <div className="App">
+      <Register />
+      <Userlist />
+    </div>
+  );
+}
+
+export default App;
---
+++ semana7/integracao-de-api/src/components/UserList/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Register from "../Register";
+
+
+
+export default class Usrlist extends React.Component{
+
+    render(){
+      return(
+          <div></div>
+      )
+    }
+}
