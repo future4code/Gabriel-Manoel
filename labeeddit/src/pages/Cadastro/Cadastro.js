5644da90bf2d3f734ce653e92c17d63b655b2b91
gabrielmanoel-hub <gabrielmanoel1401@outlook.com>
2021-10-27 13:41:03 -0300

    atualização

--- labeeddit/src/pages/Cadastro/Cadastro.js
+++ labeeddit/src/pages/Cadastro/Cadastro.js
@@ -1,11 +1,36 @@
 import React from "react"
+import { useHistory } from "react-router";
 
 
 const Cadastro = () =>{
+  const history = useHistory()
+
+  const botao = () =>{
+      history.push("/")
+  }
+
 
     return(
         <div>
-            Cadastro
+            <h1>Cadastro</h1>
+
+            <form>
+                <input 
+                
+                placeholder="nome"
+                />
+                <br/>
+                <input
+                placeholder="email"
+                />
+                <br/>
+                <input
+                placeholder="senha"
+                />
+                <br/>
+                <button>Cadastrar</button>
+            </form>
+            <button onClick={botao}>Home</button>
         </div>
     )
 }
--- labeeddit/src/pages/Login/Login.js
+++ labeeddit/src/pages/Login/Login.js
@@ -1,13 +1,55 @@
-import React from "react"
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import api from "../../api/api";
 
+const Login = () => {
+  const [form, onChange, clear] = useState({ email: "", password: ""});
 
-const Login = () =>{
+  const onSubmitForm = (event) => {
+    login() 
+    // console.log(form) 
+    event.preventDefault()
+  };
 
-    return(
-        <div>
-            Cadastro
-        </div>
-    )
-}
+  const history = useHistory()
+  
+  const handleClikc = () =>{
+      history.push("/cadastro")
+  }
+
+  const login = () =>{
+     api
+      .post(`${form}`)
+      .then((resposta)=>resposta.data)
+  }
+ 
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={onSubmitForm}>
+        <input
+          name="email"
+          value={form.email}
+          onChange={onChange}
+          required
+          type="email"
+          placeholder="email"
+        />
+        <br />
+        <input
+          name="password"
+          value={form.password}
+          onChange={onChange}
+          required
+          type="password"
+          placeholder="senha"
+        />
+        <br />
+        <button type={"submit"}>Entrar</button>
+      </form>
+      <button onClick={handleClikc}>Cadastrar</button>
+    </div>
+  );
+};
 
 export default Login;
