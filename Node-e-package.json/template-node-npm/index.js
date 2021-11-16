e9e679dec315cdfda12bf93f2f1e3f82a6e74735
gabrielmanoel-hub <gabrielmanoel1401@outlook.com>
2021-11-16 15:55:43 -0300

    node.js

---
+++ Node-e-package.json/template-node-npm/index.js
@@ -0,0 +1,32 @@
+// =================== EXERCICIO 01 ==============================
+// a) Resposta: ==> process.argv
+ 
+
+//B) Resposta
+
+// let nome= "pedro"
+// let idade = 70
+
+
+// console.log(`Olá, ${nome}! Você tem ${idade} anos.`)
+
+
+//C) Resposta 
+// console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${idade + 7}`)
+
+// =================== EXERCICIO 02 ============================
+// let cal = process.argv[2]
+// let  calculo1 = Number( process.argv[2])
+// let  calculo2 = Number( process.argv[3])
+
+// let  resposta = () =>{
+//      calculo1 + calculo2
+// }
+
+// console.log(resposta())
+
+// ==================== exercicio03 ============================
+// const tarefa = require("./tarefa")
+
+// console.log(process.argv)
+
