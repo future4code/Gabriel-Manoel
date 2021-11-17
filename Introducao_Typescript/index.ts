6b60ef0a03768e433cf1d1661ebb3109dccf92df
gabrielmanoel-hub <gabrielmanoel1401@outlook.com>
2021-11-17 16:28:49 -0300

    typescript

---
+++ Introducao_Typescript/index.ts
@@ -0,0 +1,58 @@
+//========================= EXERCÍCIO 1 ==============================
+
+// A)
+// O tipo 'number' não pode ser atribuído ao tipo 'string'
+//const minhaString: string = 50 
+
+//B)
+//const meuNumero: number = 50
+
+// type dados = {
+//     name: "paulo",
+//     idae: 40
+// }
+
+
+//C)
+// const pessoa: {name: string, idade:number, corFavorita: string } = {
+//     name: "Paulo",
+//     idade: 20,
+//     corFavorita: "green"
+// }
+
+// const pessao2: {name: string, idade: number, corFavorita: string} = {
+//     name : "Pedro",
+//     idade: 30,
+//     corFavorita: "branca"
+// }
+
+// const pessoa3: {name: string, idade: number, corFavorita: string} = {
+//     name: "marcos",
+//     idade: 50,
+//     corFavorita: "marrom"
+// }
+
+//D)
+// enum arco_iris {
+//    violeta = "violeta",
+//    anil = "anil",
+//    azul = "azul",
+//    verde = "verde",
+//    amarelo = "amarelo",
+//    laranja = "laranja",
+//    vermelho = "vermelho" 
+// }
+
+// type novoArcoIris = {
+//     class: arco_iris,
+//    violeta: arco_iris,
+//    azul: arco_iris
+// }
+
+// const criandoNovoArcoIris: novoArcoIris = {
+//     class: arco_iris.verde,
+//     violeta: arco_iris.violeta,
+//     azul: arco_iris.azul
+// }
+
+// ======================== exercício 2 ============
